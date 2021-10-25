33f2e2be21529f50e4d25a6e029c9d83f22f713b
Ubaid Nadeem <ubaidmuhammad916@gmail.com>
2021-10-26 01:03:55 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,74 @@
+var val1 = '';
+var val2 = '';
+var operator = undefined
+var ans = 0;
+
+function getvalue(a) {
+    if (operator != undefined) {
+        if(a != '+' && a != '-' && a != '*' && a != '/' && a != '=')   
+        val2 = val2 + a
+            // console.log(val2); 
+            document.getElementById('ans').innerText=val2
+      
+    }
+
+    else  if(a != '+' && a != '-' && a != '*' && a != '/' && a != '=') {
+        val1 = val1 + a
+        // console.log(val1)
+        document.getElementById('ans').innerText=val1
+
+    }
+
+
+
+    if (a == '+') {
+        operator = "+"
+        // console.log(operator)
+        document.getElementById('ans').innerText=operator
+
+    }
+    else if (a == '-') {
+        operator = "-"
+        document.getElementById('ans').innerText=operator
+        // console.log(operator)
+
+    }
+    else if (a == '*') {
+        operator = "*"
+        document.getElementById('ans').innerText=operator
+        // console.log(operator)
+
+    }
+    else if (a == '/') {
+        operator = "/"
+        // console.log(operator)
+        document.getElementById('ans').innerText=operator
+
+    }
+
+    if (a == '=') {
+        if (operator == '+') {
+            ans = +val1 + +val2
+            // console.log(ans)
+            document.getElementById('ans').innerText=ans
+        }
+        else if (operator == '-') {
+            ans = +val1 - +val2
+            // console.log(ans)
+            document.getElementById('ans').innerText=ans
+        }
+        else if (operator == '*') {
+            ans = +val1 * +val2
+            // console.log(ans)
+            document.getElementById('ans').innerText=ans
+        }
+        if (operator == '/') {
+            ans = +val1 / +val2
+            document.getElementById('ans').innerText=ans
+            // console.log(ans)
+        }
+    }
+}
+function erase(){
+    document.getElementById('ans').innerHTML=0;
+}
